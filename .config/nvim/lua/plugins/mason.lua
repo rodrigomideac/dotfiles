649adcf9689b29e548f33b8df519e48698d7d069
Rodrigo Midea <rodrigomidea@gmail.com>
2026-02-15 21:27:09 -0300

    Update mason tools

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -20,6 +20,23 @@ return {
       local is_headless = #vim.api.nvim_list_uis() == 0
       return {
         ensure_installed = {
+        -- LSP
+        "clangd", -- C/C++ language server
+        "docker-compose-language-service", -- Docker Compose language server
+        "dockerfile-language-server", -- Dockerfile language server
+        "gopls", -- Go language server
+        "helm-ls", -- Helm language server
+        "jdtls", -- Java language server
+        "json-lsp", -- JSON language server
+        "lua-language-server", -- Lua language server
+        "marksman", -- Markdown language server
+        "neocmakelsp", -- CMake language server
+        "pyright", -- Python language server
+        "ruff", -- Python linter/formatter (LSP)
+        "taplo", -- TOML language server
+        "vtsls", -- TypeScript/JavaScript language server
+        "yaml-language-server", -- YAML language server
+
         -- Formatters
         "stylua", -- Lua formatter
         "shfmt", -- Shell formatter
@@ -32,14 +49,24 @@ return {
         -- Linters
         "shellcheck", -- Shell linter
         "eslint_d", -- JS/TS linter (daemon)
+        "golangci-lint", -- Go linter
         "markdownlint-cli2", -- Markdown linter
         "hadolint", -- Dockerfile linter
         "yamllint", -- YAML linter
+        "cmakelang", -- CMake formatter/linter
+        "cmakelint", -- CMake linter
 
         -- DAP
         "debugpy", -- Python debugger
         "delve", -- Go debugger
         "codelldb", -- Rust/C/C++ debugger
+        "java-debug-adapter", -- Java debugger
+        "java-test", -- Java test runner
+        "js-debug-adapter", -- JS/TS debugger
+
+        -- Tools
+        "gitui", -- Git TUI
+        "markdown-toc", -- Markdown TOC generator
       },
       auto_update = false,
       run_on_start = not is_headless,
