db3c2e00ed1bd697af82f9d478eb527df306102f
Rodrigo <rodrigomidea@gmail.com>
2026-03-22 00:11:43 +0000

    Fix noice in tmux

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    -- Disable blend/transparency which can cause invisible floating windows in tmux
+    views = {
+      mini = {
+        win_options = { winblend = 0 },
+      },
+      popup = {
+        win_options = { winblend = 0 },
+      },
+      notify = {
+        win_options = { winblend = 0 },
+      },
+    },
+    commands = {
+      history = {
+        filter = {
+          any = {
+            { event = "notify" },
+            { error = true },
+            { warning = true },
+            { event = "msg_show", kind = { "", "echo", "echomsg", "lua_print", "list_cmd" } },
+            { event = "lsp", kind = "message" },
+          },
+        },
+      },
+    },
+  },
+}
